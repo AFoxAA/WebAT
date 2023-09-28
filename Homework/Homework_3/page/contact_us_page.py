ad488af98f68bddbc53020929bfc07ef9f71ea21
FoxA23 <afoxaa96@gmail.com>
2023-09-29 01:54:40 +0300

    Homework
    Решение задачи в папке Homework_3:
    - добавление пакета page:
            -  __init__.py
            -  base_page.py
            -  contact_us_page.py
            -  homepage.py
            -  login_page.py
            -  post_creation_page.py
            -  post_page.py
    
    - screenshot (директория со скринами после выполнения тестов)
    - config.yaml
    - conftest.py
    - log.txt
    - pytest.ini
    - test_negative.py
    - test_positive.py

---
+++ Homework/Homework_3/page/contact_us_page.py
@@ -0,0 +1,57 @@
+import time
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+import logging
+import yaml
+
+with open('config.yaml') as file:
+    file_data = yaml.safe_load(file)
+
+
+class ContactUsPageLocators:
+    LOCATOR_FORM_NAME: tuple[str, str] = (By.XPATH, file_data['form_name'])
+    LOCATOR_FIELD_YOUR_NAME: tuple[str, str] = (By.XPATH, file_data['field_your_name'])
+    LOCATOR_FIELD_YOUR_EMAIL: tuple[str, str] = (By.XPATH, file_data['field_your_email'])
+    LOCATOR_FIELD_YOUR_CONTENT: tuple[str, str] = (By.XPATH, file_data['field_your_content'])
+    LOCATOR_CONTACT_US_BUTTON: tuple[str, str] = (By.XPATH, file_data['contact_us_button'])
+
+
+class ContactUsHelper(BasePage):
+    def finding_the_form_title_text(self) -> str:
+        form_name = self.find_element(ContactUsPageLocators.LOCATOR_FORM_NAME, time=2).text
+
+        logging.info(f'Нахождение текста "{form_name}" в поле {ContactUsPageLocators.LOCATOR_FORM_NAME[1]}')
+
+        return form_name
+
+    def fill_your_name_field(self, text_your_name: str):
+        logging.info(f'Отправлять имя элементу {ContactUsPageLocators.LOCATOR_FIELD_YOUR_NAME[1]}')
+
+        self.find_element(ContactUsPageLocators.LOCATOR_FIELD_YOUR_NAME).send_keys(text_your_name)
+
+    def fill_your_email_field(self, text_your_email: str) -> None:
+        logging.info(f'Отправлять email элементу {ContactUsPageLocators.LOCATOR_FIELD_YOUR_NAME[1]}')
+
+        self.find_element(ContactUsPageLocators.LOCATOR_FIELD_YOUR_EMAIL).send_keys(text_your_email)
+
+    def fill_your_content_field(self, text_content: str) -> None:
+        logging.info(f'Отправлять content элементу {ContactUsPageLocators.LOCATOR_FIELD_YOUR_CONTENT[1]}')
+
+        self.find_element(ContactUsPageLocators.LOCATOR_FIELD_YOUR_CONTENT).send_keys(text_content)
+
+    def click_on_the_contact_us_button(self):
+        logging.info('Нажатие кнопки "CONTACT US"')
+
+        self.find_element(ContactUsPageLocators.LOCATOR_CONTACT_US_BUTTON).click()
+
+    def receiving_text_from_alert(self):
+        logging.info('Получаем текст из alert')
+
+        time.sleep(2)
+        alert = self.driver.switch_to.alert
+        result = alert.text
+
+        logging.info('Закрываем alert')
+        alert.accept()
+
+        return result
---
+++ Homework/Homework_3/page/login_page.py
@@ -0,0 +1,61 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+import logging
+import yaml
+
+with open('config.yaml') as file:
+    file_data = yaml.safe_load(file)
+
+
+class LoginPageLocators:
+    LOCATOR_LOGIN_FIELD: tuple[str, str] = (By.XPATH, file_data['username'])
+    LOCATOR_PASSWORD_FIELD: tuple[str, str] = (By.XPATH, file_data['password'])
+    LOCATOR_BUTTON_LOGIN: tuple[str, str] = (By.CSS_SELECTOR, file_data['button_login'])
+    LOCATOR_ERROR_MESSAGE: tuple[str, str] = (By.XPATH, file_data['error'])
+    LOCATOR_INPUT_FIELD_HEIGHT: tuple[str, str] = (By.CSS_SELECTOR, file_data['input_field_height'])
+    LOCATOR_LOGIN_BUTTON_COLOR: tuple[str, str] = (By.XPATH, file_data['login_button_color'])
+
+
+class LoginPageHelper(BasePage):
+    def enter_login(self, login: str) -> None:
+        logging.info(f'Отправлять логин элементу {LoginPageLocators.LOCATOR_LOGIN_FIELD[1]}')
+
+        login_field = self.find_element(LoginPageLocators.LOCATOR_LOGIN_FIELD)
+        login_field.clear()
+        login_field.send_keys(login)
+
+    def enter_password(self, password: str) -> None:
+        logging.info(f'Отправлять пароль элементу {LoginPageLocators.LOCATOR_PASSWORD_FIELD[1]}')
+
+        login_field = self.find_element(LoginPageLocators.LOCATOR_PASSWORD_FIELD)
+        login_field.clear()
+        login_field.send_keys(password)
+
+    def click_on_the_login_button(self) -> None:
+        logging.info('Нажатие кнопки входа в личный кабинет пользователя')
+
+        self.find_element(LoginPageLocators.LOCATOR_BUTTON_LOGIN).click()
+
+    def checking_error_text(self) -> str:
+        error_massage = self.find_element(LoginPageLocators.LOCATOR_ERROR_MESSAGE, time=2)
+        text_error = error_massage.text
+
+        logging.info(f'Нахождение текста {text_error} в поле ошибки {LoginPageLocators.LOCATOR_ERROR_MESSAGE[1]}')
+
+        return text_error
+
+    def input_field_height(self) -> str:
+        logging.info('Проверка высоты полей логина и пароля')
+
+        field_height = self.find_element(LoginPageLocators.LOCATOR_INPUT_FIELD_HEIGHT)
+        result = field_height.value_of_css_property('height')
+
+        return result
+
+    def login_button_color(self) -> str:
+        logging.info('Проверка цвета кнопки входа в личный кабинет пользователя')
+
+        button_color = self.find_element(LoginPageLocators.LOCATOR_LOGIN_BUTTON_COLOR)
+        result = button_color.value_of_css_property('color')
+
+        return result
